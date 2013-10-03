cfc065db7395659398e04cba23bfb6834207add3
Andy MacKinlay <andrew.mackinlay@nicta.com.au>
2013-10-03 14:29:59 +1000

    refactored example code into a separate file

--- src/main/java/com/nicta/hls/vlabclient/VLabRestClient.java
+++ src/main/java/com/nicta/hls/vlabclient/VLabRestClient.java
@@ -250,39 +250,4 @@ public class VLabRestClient {
 				.request(contType).accept(contType)
 				.header("X-API-KEY", apiKey);
 	}
-
-	
-	public static void main(String[] args) {
-		String serverUri = args[0];
-		String apiKey = args[1];
-		String itemListId = args[2];
-
-		VLabRestClient client = new VLabRestClient(serverUri, apiKey);
-		try {
-			System.out.println(client.getItemListJson(itemListId));
-			ItemList il = client.getItemList(itemListId);
-			System.out.println(String.format("Found %d items", il.numItems())); 
-			for (CatalogItem ci : il.getCatalogItems()) {
-				System.out.println("\nURI:" + ci.getUri());
-				System.out.println("\nPRIMARY TEXT:\n" + ci.primaryText());
-				System.out.println("\nMETADATA:");
-				for (Map.Entry<String, String> entry : ci.getMetadata().entrySet()) {
-					System.out.println(entry.getKey() + ": " + entry.getValue());
-				}
-				System.out.println("\nDOCS:");
-				for (Document doc : ci.documents()) {
-					System.out.println("\tTEXT URL: " + doc.getRawTextUrl());
-					System.out.println("\tSIZE: " + doc.getSize());
-					System.out.println("\tTYPE: " + doc.getType());
-					String text = doc.rawText();
-					if (text.length() > 5000) 
-						text = text.substring(0, 3000) + "…";
-					System.out.println("\tCONTENT: " + text);
-					System.out.println("\t==================");
-				}
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
 }
