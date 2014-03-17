17e47eb8e73bc3062e8ddf1ad71011ce6cd5a9b7
Andrew MacKinlay <am@akmy.net>
2014-03-17 14:40:40 +1100

    took out unneeded todos

--- src/main/java/com/nicta/vlabclient/RestClientException.java
+++ src/main/java/com/nicta/vlabclient/RestClientException.java
@@ -3,22 +3,18 @@ package com.nicta.vlabclient;
 public class RestClientException extends Exception {
 
 	public RestClientException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public RestClientException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RestClientException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RestClientException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
