4c01dd128694046b3585bd886b727cbaefb8bbc5
Andrew MacKinlay <am@akmy.net>
2014-04-10 16:34:15 +1000

    updated test cases to handle API changes in Alveo

--- src/test/java/com/nicta/vlabclient/RestClientBaseTest.java
+++ src/test/java/com/nicta/vlabclient/RestClientBaseTest.java
@@ -11,6 +11,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -35,12 +37,12 @@ public abstract class RestClientBaseTest {
 		Assert.assertEquals(2, il.numItems());
 		List<Item> items = il.getCatalogItems();
 		Assert.assertEquals(il.numItems(), items.size());
-		Assert.assertEquals(items.get(1).getUri(), newRestClient().getItem("gcsause:GCSAusE07").getUri());
+		Assert.assertEquals(items.get(1).getUri(), newRestClient().getItem("gcsause/GCSAusE07").getUri());
 	}
 
 	@Test
 	public void fetchItem() throws HCSvLabException {
-		Item item0 = newRestClient().getItem("gcsause:GCSAusE07");
+		Item item0 = newRestClient().getItem("gcsause/GCSAusE07");
 		String item0text = item0.primaryText();
 		Assert.assertTrue(item0text.startsWith("Andy’s starting"));
 		Assert.assertTrue(item0text.endsWith("know"));
@@ -48,14 +50,14 @@ public abstract class RestClientBaseTest {
 
 	@Test
 	public void fetchAnnotations() throws HCSvLabException {
-		Item item = newRestClient().getItem("gcsause:GCSAusE07");
+		Item item = newRestClient().getItem("gcsause/GCSAusE07");
 		List<Annotation> anns = item.getAnnotations();
 		Assert.assertTrue(anns.size() > 20);
 	}
 
 	@Test
 	public void checkAnnotations() throws HCSvLabException {
-		Item item = newRestClient().getItem("gcsause:GCSAusE07");
+		Item item = newRestClient().getItem("gcsause/GCSAusE07");
 		List<Annotation> anns = item.getAnnotations();
 		TextAnnotation firstSpkrAnn = (TextAnnotation) anns.get(9);
 		Assert.assertEquals(93, firstSpkrAnn.getStartOffset());
@@ -63,14 +65,19 @@ public abstract class RestClientBaseTest {
 		String docText = item.primaryText();
 		String annText = docText.substring(firstSpkrAnn.getStartOffset(), firstSpkrAnn.getEndOffset());
 		Assert.assertTrue(annText.startsWith("having") && annText.replaceAll(" *$", "").endsWith("nap"));
-		Assert.assertEquals("http://hcsvlab.org/bare-types/speaker", firstSpkrAnn.getType());
-		TextAnnotation lastAnn = (TextAnnotation) anns.get(anns.size() - 1);
+		Assert.assertEquals("http://ns.ausnc.org.au/schemas/annotation/ice/speaker", firstSpkrAnn.getType());
+		TextAnnotation lastAnn = (TextAnnotation) Collections.max(anns, new Comparator<Annotation>() {
+			@Override
+			public int compare(Annotation annotation, Annotation annotation2) {
+				return (int) Math.signum(annotation.getEnd() - annotation2.getEnd());
+			}
+		});
 		Assert.assertEquals(docText.length(), lastAnn.getEndOffset());
 	}
 
 	@Test
 	public void uploadAnnotations() throws HCSvLabException {
-		Item i0 = newRestClient().getItem("gcsause:GCSAusE07");
+		Item i0 = newRestClient().getItem("gcsause/GCSAusE07");
 		List<Annotation> anns = new ArrayList<Annotation>();
 		anns.add(new TextRestAnnotation("entity", "proper-name", 0, 4));
 		anns.add(new TextRestAnnotation("pos", "NNP", 0, 4));
